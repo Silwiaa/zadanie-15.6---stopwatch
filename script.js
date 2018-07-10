c75900886b5fe28bdce2451877fbc02eccc86ee6
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-07-10 20:46:17 +0200

    add methods to stop and start buttons

--- script.js
+++ script.js
@@ -6,4 +6,24 @@ class Stopwatch {
         this.reset();
         this.print(this.times);
     }
+    
+    //RESET METHOD
+    reset(){
+        this.times={
+            minutes:0,
+            seconds:0,
+            miliseconds:0,
+        };
+    }
 }
+
+// NEW INSTANCE
+const stopwatch = new Stopwatch (document.querySelector('.stopwatch'));
+
+// START BUTTON
+let startButton = document.getElementById('start');
+startButton.addEventListener('click', ()=>stopwatch.start());
+
+// STOP BUTTON
+let stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', ()=>stopwatch.stop());
